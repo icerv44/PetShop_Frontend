5acedb3aee9944e18d5f489dbdc67d49b2680e70
icerv44 <icerv44@hotmail.com>
2022-08-04 02:16:55 +0700

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,11 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+function Router() {
+  return (
+    <Route>
+      <Route path="" element={""} />
+    </Route>
+  );
+}
+
+export default Router;
